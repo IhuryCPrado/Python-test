e3b7d5701a7f181bd9ef70eb18e92a618201bbc7
Ihury Prado <ihurycprado@gmail.com>
2025-02-24 21:11:44 -0300

    Atualizações do dia 24/02/2025

--- calculadora.py
+++ calculadora.py
@@ -1,2 +1,34 @@
 def soma(x,y):
+    """
+    Soma x e y
+    
+    >>> soma(10, 25.44)
+    35.44
+
+    >>> soma(10, 20)
+    30
+
+    >>> soma('10', 12)
+    Traceback (most recent call last):
+    ...
+    AssertionError: x precisa ser int ou float
+    """
+    assert isinstance(x, (int, float)), 'x precisa ser int ou float'
+    assert isinstance(y, (int, float)), 'y precisa ser int ou float'
     return x + y
+
+def subtrai(x, y):
+    """Subtrai x e y
+    
+    >>> subtrai(10, 5)
+    5
+
+    >>> subtrai('10', 5)
+    """
+    assert isinstance(x, (int, float)), 'x precisa ser int ou float'
+    assert isinstance(y, (int, float)), 'y precisa ser int ou float'
+    return x - y
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod(verbose=True)
--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from calculadora import soma
 
-print(soma(10,20))
-print(soma(0,20))
-print(soma(10.5,20))
-print(soma(10,20))
+# print(soma(10,20))
+# print(soma(0,20))
+# print(soma(10.5,20))
+# print(soma(10,20))
+try:
+    print(soma('15',15))
+except AssertionError as e:
+    print(f'Conta inválida: {e}')
